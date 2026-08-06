6f10718773c4f116a99198621920c0ebf51d0dfd
nabaelhaq-gif <nabaelhaq@gmail.com>
2026-08-06 11:05:56 +0700

    menambahkan void searchdata

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -181,4 +181,18 @@ public:
         }
     }
 
-    
+    void searchData()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        int rollNo;
+        cout << "\nEnter the roll number to search: ";
+        cin >> rollNo;
+
+        Node *current = START;
+
+        
