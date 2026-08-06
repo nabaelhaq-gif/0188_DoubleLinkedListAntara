6dfadabf4ad09c08f35b4ca21a6c13f4906fb9d3
nabaelhaq-gif <nabaelhaq@gmail.com>
2026-08-06 10:48:55 +0700

    menambahkan class private dan public

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -10,3 +10,16 @@ public:
     Node *next;
     Node *prev;
 };
+
+class DoubleLinkedList
+{
+private:
+    Node *START;
+
+public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+
+    
